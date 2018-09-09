6a92021a7e43e72a6d7390513a6c406b9d9a1aa8
Md.Aminul Islam Fahim <cipfahim1999@gmail.com>
2018-09-09 22:16:01 +0600

    customer factory added

---
+++ database/factories/CustomerFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Customer::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'phone' => $faker->phoneNumber,
+        'address' => $faker->address,
+        'total' => $faker->randomFloat($nbMaxDecimals = 2, $min = 100.00, $max = 1000.00)
+    ];
+});
