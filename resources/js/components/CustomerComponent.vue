6d24394e9e8869b74b59dbda58fdc2adf960149a
Md.Aminul Islam Fahim <cipfahim1999@gmail.com>
2018-09-09 21:54:43 +0600

    init commit

---
+++ resources/js/components/CustomerComponent.vue
@@ -0,0 +1,53 @@
+<template>
+   <div id="customer">
+       <div class="row justify-content-center">
+           <div class="col-md-12">
+               <div class="card">
+                   <div class="card-header">Customers</div>
+
+                   <div class="card-body">
+                       <div class="table-responsive">
+                           <table class="table table-hover table-bordered table-striped">
+                               <thead>
+                               <tr>
+                                   <th scope="col">#</th>
+                                   <th scope="col">First</th>
+                                   <th scope="col">Last</th>
+                                   <th scope="col">Handle</th>
+                               </tr>
+                               </thead>
+                               <tbody>
+                               <tr>
+                                   <th scope="row">1</th>
+                                   <td>Mark</td>
+                                   <td>Otto</td>
+                                   <td>@mdo</td>
+                               </tr>
+                               <tr>
+                                   <th scope="row">2</th>
+                                   <td>Jacob</td>
+                                   <td>Thornton</td>
+                                   <td>@fat</td>
+                               </tr>
+                               <tr>
+                                   <th scope="row">3</th>
+                                   <td colspan="2">Larry the Bird</td>
+                                   <td>@twitter</td>
+                               </tr>
+                               </tbody>
+                           </table>
+                       </div>
+                   </div>
+               </div>
+           </div>
+       </div>
+   </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
---
+++ resources/views/customers.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <customer-component></customer-component>
+</div>
+@endsection
